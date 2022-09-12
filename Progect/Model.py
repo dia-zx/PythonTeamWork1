3110c2811be2d357ad5ed6b44f3dd35e06b8c226
dia_zx <dia_zx@mail.ru>
2022-09-12 17:37:29 +0300

    Model.py done

--- Progect/Model.py
+++ Progect/Model.py
@@ -11,14 +11,41 @@ def Init(items : dict):
 # возврат списка записей (словарь) удовлетворяющих фильтру
 def GetItems(filter : str):
     filter_items = {}
-    None
+    if filter == "":
+        return Items
+    for key in Items.keys():
+        for item in Items[key]:
+            if item.find(filter) >= 0:
+                filter_items.update({key : Items[key]})
+                break
     return filter_items
 
 
+# Добавление записи в словарь (если ID == 0) иначе изменение существующей
 def AddItem(ls : list):
-    None
+    NewID = ls[0]
+    if ls[0] == 0:
+        NewID = 0
+        while True:
+            if NewID not in Items.keys():
+                break
+            NewID += 1
+    Items.update({NewID : ls[1:]})
+            
+    
 
+#Удаление записи по ID
+def DeleteItem(id : int):
+    return Items.pop(id, None) != None
+   
 
-def DeleteItem(id):
-    None
-    return True
+# ms = {1: ["Иванов", "Сергеевич", "+7899"]}
+# ms.update({2: ["Петров", "Иванович", "+7890"]})
+# Init(ms)
+
+# AddItem([10, "sdfdf", "123"])
+# #print(DeleteItem(1))
+# print(GetItems(""))
+
+# # st = "fdzxc"
+# # print(st.find("zxc"))
